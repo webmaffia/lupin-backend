61c9adddbdcff8aa1b03f6245cac9e1417ff635b
ankit-tomer2 <ankittomer@gmail.com>
2026-01-22 17:00:57 +0530

    azure config

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,7 +1,41 @@
 module.exports = [
+  {
+    name: "strapi::security",
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          "connect-src": ["'self'", "https:"],
+          "img-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            "https://market-assets.strapi.io", // Required for Strapi >= 4.10.6, you can remove it otherwise
+            /**
+             * Note: If using a STORAGE_URL replace `https://${process.env.STORAGE_ACCOUNT}.blob.core.windows.net` w/ process.env.STORAGE_URL
+             * If using a CDN URL make sure to include that url in the CSP headers process.env.STORAGE_CDN_URL
+             */
+            `https://${process.env.STORAGE_ACCOUNT}.blob.core.windows.net`,
+          ],
+          "media-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            "dl.airtable.com", // Required for Strapi < 4.10.6, you can remove it otherwise
+            /**
+             * Note: If using a STORAGE_URL replace `https://${process.env.STORAGE_ACCOUNT}.blob.core.windows.net` w/ process.env.STORAGE_URL
+             * If using a CDN URL make sure to include that url in the CSP headers process.env.STORAGE_CDN_URL
+             */
+            `https://${process.env.STORAGE_ACCOUNT}.blob.core.windows.net`,
+
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
